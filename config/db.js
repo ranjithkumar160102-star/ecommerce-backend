89d61b18cc0311cb2380d7cf634f96b477e5b0dc
[200~ranjithkumar160102-star~ <ranjithkumar160102@gmail.com>
2026-05-22 23:49:21 +0530

    fixed mongodb atlas connection

--- config/db.js
+++ config/db.js
@@ -1,25 +1,13 @@
 const mongoose = require("mongoose");
 
-
-
 const connectDB = async () => {
-
   try {
-
-    await mongoose.connect(
-      "mongodb://127.0.0.1:27017/ecommerceDB"
-    );
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB Connected");
-
   } catch (error) {
-
     console.log(error.message);
-
   }
-
 };
-
-
 
 module.exports = connectDB;
