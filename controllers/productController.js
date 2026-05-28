77ccfb0e34e12a87d043752d9f64c5c0698c05bc
[200~ranjithkumar160102-star~ <ranjithkumar160102@gmail.com>
2026-05-28 09:43:56 +0530

    fixed product controller error

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,171 +3,144 @@ const Product = require("../models/product");
 // Create Product
 const createProduct = async (req, res) => {
 
-try {
+  try {
 
-```
-const product = await Product.create(req.body);
+    const product = await Product.create(req.body);
 
-res.status(201).json({
-  message: "Product Created Successfully",
-  product,
-});
-```
+    res.status(201).json({
+      message: "Product Created Successfully",
+      product,
+    });
 
-} catch (error) {
+  } catch (error) {
 
-```
-res.status(500).json({
-  message: error.message,
-});
-```
+    res.status(500).json({
+      message: error.message,
+    });
 
-}
+  }
 
 };
 
 // Get All Products
 const getProducts = async (req, res) => {
 
-try {
+  try {
 
-```
-const search = req.query.search || "";
+    const search = req.query.search || "";
+    const category = req.query.category || "";
 
-const category = req.query.category || "";
+    let query = {};
 
-let query = {};
+    // Search
+    if (search) {
 
+      query.name = {
+        $regex: search,
+        $options: "i",
+      };
 
+    }
 
-// Search by Product Name
-if (search) {
+    // Filter
+    if (category) {
 
-  query.name = {
-    $regex: search,
-    $options: "i",
-  };
+      query.category = category;
 
-}
+    }
 
+    const products = await Product.find(query);
 
+    res.status(200).json(products);
 
-// Filter by Category
-if (category) {
+  } catch (error) {
 
-  query.category = category;
+    res.status(500).json({
+      message: error.message,
+    });
 
-}
-
-
-
-const products = await Product.find(query);
-
-res.status(200).json(products);
-```
-
-} catch (error) {
-
-```
-res.status(500).json({
-  message: error.message,
-});
-```
-
-}
+  }
 
 };
 
 // Get Single Product
 const getSingleProduct = async (req, res) => {
 
-try {
+  try {
 
-```
-const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id);
 
-if (!product) {
+    if (!product) {
 
-  return res.status(404).json({
-    message: "Product Not Found",
-  });
+      return res.status(404).json({
+        message: "Product Not Found",
+      });
 
-}
+    }
 
-res.status(200).json(product);
-```
+    res.status(200).json(product);
 
-} catch (error) {
+  } catch (error) {
 
-```
-res.status(500).json({
-  message: error.message,
-});
-```
+    res.status(500).json({
+      message: error.message,
+    });
 
-}
+  }
 
 };
 
 // Update Product
 const updateProduct = async (req, res) => {
 
-try {
+  try {
 
-```
-const updatedProduct = await Product.findByIdAndUpdate(
-  req.params.id,
-  req.body,
-  { new: true }
-);
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
 
-res.status(200).json({
-  message: "Product Updated Successfully",
-  updatedProduct,
-});
-```
+    res.status(200).json({
+      message: "Product Updated Successfully",
+      updatedProduct,
+    });
 
-} catch (error) {
+  } catch (error) {
 
-```
-res.status(500).json({
-  message: error.message,
-});
-```
+    res.status(500).json({
+      message: error.message,
+    });
 
-}
+  }
 
 };
 
 // Delete Product
 const deleteProduct = async (req, res) => {
 
-try {
+  try {
 
-```
-await Product.findByIdAndDelete(req.params.id);
+    await Product.findByIdAndDelete(req.params.id);
 
-res.status(200).json({
-  message: "Product Deleted Successfully",
-});
-```
+    res.status(200).json({
+      message: "Product Deleted Successfully",
+    });
 
-} catch (error) {
+  } catch (error) {
 
-```
-res.status(500).json({
-  message: error.message,
-});
-```
+    res.status(500).json({
+      message: error.message,
+    });
 
-}
+  }
 
 };
 
 module.exports = {
-createProduct,
-getProducts,
-getSingleProduct,
-updateProduct,
-deleteProduct,
+  createProduct,
+  getProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
 };
